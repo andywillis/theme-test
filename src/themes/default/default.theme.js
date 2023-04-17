b8c70506bbc18053cb646bca3b79b3c0df124cc7
Andy Willis <awillis@fastmail.fm>
2023-04-17 12:19:00 +0100

    Fixed colors

--- src/themes/default/default.theme.js
+++ src/themes/default/default.theme.js
@@ -1,9 +1,7 @@
 import layout from './layout';
 import typography from './typography';
-// import color from './color';
 
 const defaultTheme = {
-  // ...color,
   ...layout,
   ...typography
 };
--- src/themes/default/layout.js
+++ src/themes/default/layout.js
@@ -1,47 +1,17 @@
 export default {
 
-  // Variables
-
-  // Container padding
+  // Padding
   '--container-padding': '1rem',
-  '--container-padding-top': '1rem',
-  '--container-padding-right': '1rem',
-  '--container-padding-bottom': '1rem',
-  '--container-padding-left': '1rem',
-
-  // Container margin
-  '--container-margin': '1rem',
-  '--container-margin-top': '1rem',
-  '--container-margin-right': '1rem',
-  '--container-margin-bottom': '1rem',
-  '--container-margin-left': '1rem',
-
-  // Box padding
   '--box-padding': '0.25rem',
-  '--box-padding-top': '0.25rem',
-  '--box-padding-right': '0.25rem',
-  '--box-padding-bottom': '0.25rem',
-  '--box-padding-left': '0.25rem',
 
-  // Box border radius
-  '--box-border-radius': '5px',
-  '--box-border-radius-top': '5px',
-  '--box-border-radius-right': '5px',
-  '--box-border-radius-bottom': '5px',
-  '--box-border-radius-left': '5px',
-
-  // Box border radius
-  '--box-border-width': '2px',
-  '--box-border-top-width': '2px',
-  '--box-border-right-width': '2px',
-  '--box-border-bottom-width': '2px',
-  '--box-border-left-width': '2px',
-
-  // Box margin
+  // Margins
+  '--container-margin': '1rem',
   '--box-margin': '0.5rem',
-  '--box-margin-top': '0.5rem',
-  '--box-margin-right': '0.5rem',
-  '--box-margin-bottom': '0.5rem',
-  '--box-margin-left': '0.5rem'
+
+  // Border
+  '--container-border-radius': '5px',
+  '--container-border-width': '2px',
+  '--box-border-radius': '5px',
+  '--box-border-width': '2px',
 
 };
