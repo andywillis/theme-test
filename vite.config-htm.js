92617494ea604d2f674b7b2f709accdf57aa8260
Andy Willis <awillis@fastmail.fm>
2023-04-19 01:42:23 +0100

    Updated

---
+++ vite.config-htm.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
+import postcssNesting from 'postcss-nesting';
+
+export default defineConfig({
+  root: 'src',
+  plugins: [
+    preact()
+  ],
+  server: {
+    open: true,
+    port: 3000
+  },
+  resolve: {
+    alias: {
+      react: 'preact/compat'
+    }
+  },
+  css: {
+    postcss: {
+      plugins: [
+        postcssNesting
+      ]
+    }
+  },
+  build: {
+    target: 'esnext',
+    emptyOutDir: true,
+    outDir: '../build',
+    sourcemap: true
+  }
+});
